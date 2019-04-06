9aff1e2bfa15bc83566b281a57c86b107d27db47
Enotik <42745849+Enotik35@users.noreply.github.com>
2019-04-06 20:59:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 
-const appl = document.getElementById('answer');
+// Abdurahmanov Refat
+// 04.04.2019
+// Site for calculating the benefit of replacing incandescent bulbs to light-emitting diode lamps
+
+
+
+
+const appl = document.getElementById('answer');	//variable for creating answer for request
 
 function texting(ipt) {		// new function for texting in global chat
-			var textElem = document.createElement('div');
-			textElem.className = "msg "
+			var textElem = document.createElement('div');	//create element div
+			textElem.className = "msg " 
 			textElem.innerHTML = ipt;
 			appl.appendChild(textElem);
 };
@@ -13,7 +20,7 @@ let onclick = 0;
 let onsum = -1;
 
 function newlamp(){
-	onclick++;
+	onclick++;		//the variable can id and class and function name
 	var newlampo4ka = document.createElement('input');
 
 	newlampo4ka.className = "inp";
@@ -58,14 +65,79 @@ function newlamp(){
 	retrn(onsum)
 };
 function total(){
-	return;
+	texting(totalprice)
 }
 
-function calculate(x, y){
-	console.log(x,y);
-	var ipt = x + y;
-	return ipt
+function calculate(n, y){
+	const d = 50;
+	const w = 2.5;
+	switch (y){
+		case 20:
+		var price = 5;
+		var wdiod = 2.5;
+		var diodprice = 50;
+		var ipt = inerCalc(n, y, price, wdiod, diodprice, d, w); 
+		break;
+		case 40:
+		var price = 10;
+		var wdiod = 4.5;
+		var diodprice = 70;
+		var ipt = inerCalc(n, y, price, wdiod, diodprice, d, w);
+		break;
+		case 60:
+		var price = 15;
+		var wdiod = 9;
+		var diodprice = 100;
+		var ipt = inerCalc(n, y, price, wdiod, diodprice, d, w);
+		break;
+		case 75:
+		var price = 20;
+		var wdiod = 11;
+		var diodprice = 120;
+		var ipt = inerCalc(n, y, price, wdiod, diodprice, d, w);
+		break;
+		case 100:
+		var price = 30;
+		var wdiod = 13.5;
+		var diodprice = 150;
+		var ipt = inerCalc(n, y, price, wdiod, diodprice, d, w);
+		break;
+		case 150:
+		var price = 40;
+		var wdiod = 19;
+		var diodprice = 170;
+		var ipt = inerCalc(n, y, price, wdiod, diodprice, d, w);
+		break;
+		case 200:
+		var price = 50;
+		var wdiod = 27.5;
+		var diodprice = 200;
+		var ipt = inerCalc(n, y, price, wdiod, diodprice, d, w);
+		break;
+
+	};
+	totalprice = totalprice + ipt;
+	return ipt;
 };
+ let totalprice = 0;
+function inerCalc(n,y,price,wdiod,diodprice,d,w){
+	var k = n * y * d * w;
+	console.log(k);
+	var l = n * wdiod * w;
+	console.log(l);
+	var r = n * price * d;
+	console.log(r);
+	var z = n * diodprice;
+	console.log(z);
+	var p = k + r;
+	console.log(p);
+	var g = l + z;
+	console.log(g);
+	var u = p - g;
+	console.log(u);
+	return u;
+};
+
 
 function retrn(l){
 	return l;
